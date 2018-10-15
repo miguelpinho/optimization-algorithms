4f87c9a856036c4f8ef08c47045439ad8a3c256d
pedrogbmendes <pedro_mendes_123@hotmail.com>
2018-10-15 22:40:16 +0100

    begin task1

--- part1/task1.m
+++ part1/task1.m
@@ -15,39 +15,51 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear all;
 %load the workspace 
 load('dataA.mat');
 
-lambda1 = lambda(1);
+lambda1 = lambda(3);
 
 % solve optimization problem
 cvx_begin quiet
-    variable x(4, 4);
-    variable u(1, 2);
+    variable x(4, T+1);
+    variable u(2, T);
     
     %cost function
-    f1 = power( norm( E*x(tau(1)) - w(1) ),2);
-    for i=2:1:k
-        f1 = f1 + ( norm( E*x(tau(i)) - w(i) )).^2;
-    end
+    %f1 = square_pos(norm( E*x(:,tau(1)) - w(:,1)));
+    %for i=2:1:k
+    %    f1 = f1 + ( square_pos(norm( E*x(:,tau(i)) - w(:,i))) );
+    %end
     
-    f2 = ( norm( u(2) - u(1) )).^2;
-    for j=3:1:T
-        f2 = f2 + ( norm( u(j) - u(j-1) )).^2;
-    end
+    %f2 = square_pos( norm( u(2) - u(1) ) );
+    %for j=3:1:T
+    %    f2 = f2 +  ( square_pos ( norm( u(j) - u(j-1) )) );
+    %end
     
-    f = f1+(lambda1*f2);
+    %f = f1+(lambda1*f2);
     
-    minimize( f ); %como por somatorio aqui dentro? -> calculo vetorial é uma forma, ver task2
+    
+    f_waypoints = sum(square_pos( norm(E * x(:, tau) - w, 2) ) );
+    f_regularizer = sum( square_pos ( norm(col_diff(u)) ) ); % norm changes here
+    f = f_waypoints + lambda1 * f_regularizer;
+    
+    
+    minimize( f ); 
     
     %subject to
-    x(0) = initialx;
-    x(T) = finalx;
-    for t = 0:T-1
-        norm( u(t), 2) <= Umax;
+    x(:,1) == initialx;
+    x(:,T+1) == finalx;
+    
+    for t = 1:T
+        norm( u(:,t)) <= Umax;
     end
     
-    for t = 0:T-1
-        x(t+1) = A*x(t) + B*u(t);
+    for t = 1:T
+        x(:,t+1) == A*x(:,t) + B*u(:,t);
     end
+    
 cvx_end;
+
+
+plot(x(1,:),x(2,:),'.');
