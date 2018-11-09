1873eaccbdd0aad18994486845ced081bc41dcb8
Duarte Dias <DuartexDias@gmail.com>
2018-11-09 14:07:34 +0000

    task5 function working

--- part1/VariationB/task5.m
+++ part1/VariationB/task5.m
@@ -1,12 +1,11 @@
 function [ distance_to_section ] = task5( point, center, radius )
 %TASK5 Summary of this function goes here
 %   Detailed explanation goes here
-if square_pos( norm(point - center ) > radius
-    return square_pos( norm(point - center ) - radius;
-end
-elseif square_pos( norm(point - center ) <= radius
-    return 0;
-end
+    if norm(point - center ) > radius
+        distance_to_section =  norm(point - center ) - radius;
+    else
+        distance_to_section = 0;
+    end
 
 end
 
